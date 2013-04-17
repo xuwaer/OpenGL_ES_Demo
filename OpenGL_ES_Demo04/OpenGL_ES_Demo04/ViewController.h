0dbf50576c4563bbd84b9d9e9a255367aae1be36
xuwaer <xuwaer@gmail.com>
2013-04-17 11:12:46 +0800

    添加Demo04
    Demo04主要涉及内容
    
    使用VBO:顶点缓存

---
+++ OpenGL_ES_Demo04/OpenGL_ES_Demo04/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenGL_ES_Demo04
+//
+//  Created by Xukj on 4/17/13.
+//  Copyright (c) 2013 tosc-its. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OpenGL_ES_Demo04/OpenGL_ES_Demo04/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OpenGL_ES_Demo04
+//
+//  Created by Xukj on 4/17/13.
+//  Copyright (c) 2013 tosc-its. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
