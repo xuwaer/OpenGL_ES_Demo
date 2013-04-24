d58655cfdf1e0c057ca87ee7295144a26e85f963
xuwaer <xuwaer@gmail.com>
2013-04-24 13:30:26 +0800

    新建demo05
    demo05主要功能：
    1.光照公式说明
    2.光照demo演示
    3.深度缓存使用

---
+++ OpenGL_ES_Demo05/OpenGL_ES_Demo05/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenGL_ES_Demo05
+//
+//  Created by Xukj on 4/24/13.
+//  Copyright (c) 2013 tosc-its. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
